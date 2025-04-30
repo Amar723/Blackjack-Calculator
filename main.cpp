e3f82c953a18029a4b2626cf5815fe8c47e7b5b5
Amar723 <140028404+Amar723@users.noreply.github.com>
2025-05-01 00:18:36 +1000

    main file which calls the algorithm given a valid hand

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "BlackjackStrategy.h"
+
+int main() {
+    BlackjackStrategy strategy;
+    std::vector<std::string> dealer = {"6"};
+    std::vector<std::string> player = {"A", "6"};
+    int currentBet = 100;
+
+    auto result = strategy.getOptimalMove(currentBet, dealer, player);
+    std::cout << "Move: " << result.first << ", Next Bet: " << result.second << std::endl;
+
+    return 0;
+}
