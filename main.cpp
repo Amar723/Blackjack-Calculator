5cf8043ee95c5ced642fae20e681b7b9467c8daa
Amar723 <140028404+Amar723@users.noreply.github.com>
2025-05-01 00:19:24 +1000

    main function

--- main.cpp
+++ main.cpp
@@ -3,12 +3,17 @@
 
 int main() {
     BlackjackStrategy strategy;
+
+    
     std::vector<std::string> dealer = {"6"};
     std::vector<std::string> player = {"A", "6"};
+
+
     int currentBet = 100;
 
     auto result = strategy.getOptimalMove(currentBet, dealer, player);
     std::cout << "Move: " << result.first << ", Next Bet: " << result.second << std::endl;
 
     return 0;
+
 }
